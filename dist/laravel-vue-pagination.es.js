925957dbc68683d0d17c92a8c1bf292b32e58c02
Orhan Gazi Başlı <obasli@gmail.com>
2022-04-19 13:16:36 +0300

    tailwind support

--- dist/laravel-vue-pagination.es.js
+++ dist/laravel-vue-pagination.es.js
@@ -1,4 +1,4 @@
-import { resolveComponent, openBlock, createBlock, withCtx, createElementBlock, mergeProps, normalizeClass, createElementVNode, toHandlers, renderSlot, createCommentVNode, Fragment, renderList, createTextVNode, toDisplayString } from "vue";
+import { resolveComponent, openBlock, createBlock, withCtx, createElementBlock, mergeProps, normalizeClass, createElementVNode, toHandlers, createCommentVNode, Fragment, renderList, toDisplayString, renderSlot, createTextVNode } from "vue";
 const _sfc_main$1 = {
   emits: ["pagination-change-page"],
   props: {
@@ -181,6 +181,10 @@ const _sfc_main = {
       type: Boolean,
       default: false
     },
+    themeEngine: {
+      type: String,
+      default: "tailwind"
+    },
     size: {
       type: String,
       default: "default",
@@ -202,16 +206,54 @@ const _sfc_main = {
     }
   }
 };
-const _hoisted_1 = ["tabindex"];
-const _hoisted_2 = /* @__PURE__ */ createElementVNode("span", { "aria-hidden": "true" }, "\xAB", -1);
+const _hoisted_1 = { key: 0 };
+const _hoisted_2 = ["tabindex"];
 const _hoisted_3 = /* @__PURE__ */ createElementVNode("span", { class: "sr-only" }, "Previous", -1);
-const _hoisted_4 = {
+const _hoisted_4 = /* @__PURE__ */ createElementVNode("svg", {
+  class: "w-5 h-5",
+  fill: "currentColor",
+  viewBox: "0 0 20 20",
+  xmlns: "http://www.w3.org/2000/svg"
+}, [
+  /* @__PURE__ */ createElementVNode("path", {
+    "fill-rule": "evenodd",
+    d: "M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z",
+    "clip-rule": "evenodd"
+  })
+], -1);
+const _hoisted_5 = [
+  _hoisted_3,
+  _hoisted_4
+];
+const _hoisted_6 = ["tabindex"];
+const _hoisted_7 = /* @__PURE__ */ createElementVNode("span", { class: "sr-only" }, "Next", -1);
+const _hoisted_8 = /* @__PURE__ */ createElementVNode("svg", {
+  class: "w-5 h-5",
+  fill: "currentColor",
+  viewBox: "0 0 20 20",
+  xmlns: "http://www.w3.org/2000/svg"
+}, [
+  /* @__PURE__ */ createElementVNode("path", {
+    "fill-rule": "evenodd",
+    d: "M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z",
+    "clip-rule": "evenodd"
+  })
+], -1);
+const _hoisted_9 = [
+  _hoisted_7,
+  _hoisted_8
+];
+const _hoisted_10 = { key: 1 };
+const _hoisted_11 = ["tabindex"];
+const _hoisted_12 = /* @__PURE__ */ createElementVNode("span", { "aria-hidden": "true" }, "\xAB", -1);
+const _hoisted_13 = /* @__PURE__ */ createElementVNode("span", { class: "sr-only" }, "Previous", -1);
+const _hoisted_14 = {
   key: 0,
   class: "sr-only"
 };
-const _hoisted_5 = ["tabindex"];
-const _hoisted_6 = /* @__PURE__ */ createElementVNode("span", { "aria-hidden": "true" }, "\xBB", -1);
-const _hoisted_7 = /* @__PURE__ */ createElementVNode("span", { class: "sr-only" }, "Next", -1);
+const _hoisted_15 = ["tabindex"];
+const _hoisted_16 = /* @__PURE__ */ createElementVNode("span", { "aria-hidden": "true" }, "\xBB", -1);
+const _hoisted_17 = /* @__PURE__ */ createElementVNode("span", { class: "sr-only" }, "Next", -1);
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   const _component_RenderlessLaravelVuePagination = resolveComponent("RenderlessLaravelVuePagination");
   return openBlock(), createBlock(_component_RenderlessLaravelVuePagination, {
@@ -223,61 +265,106 @@ function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
     onPaginationChangePage: $options.onPaginationChangePage
   }, {
     default: withCtx((slotProps) => [
-      slotProps.computed.total > slotProps.computed.perPage ? (openBlock(), createElementBlock("ul", mergeProps({ key: 0 }, _ctx.$attrs, {
-        class: ["pagination", {
-          "pagination-sm": slotProps.size == "small",
-          "pagination-lg": slotProps.size == "large",
-          "justify-content-center": slotProps.align == "center",
-          "justify-content-end": slotProps.align == "right"
-        }]
-      }), [
-        slotProps.computed.prevPageUrl || slotProps.showDisabled ? (openBlock(), createElementBlock("li", {
-          key: 0,
-          class: normalizeClass(["page-item pagination-prev-nav", { "disabled": !slotProps.computed.prevPageUrl }])
-        }, [
-          createElementVNode("a", mergeProps({
-            class: "page-link",
-            href: "#",
-            "aria-label": "Previous",
-            tabindex: !slotProps.computed.prevPageUrl && -1
-          }, toHandlers(slotProps.prevButtonEvents)), [
-            renderSlot(_ctx.$slots, "prev-nav", {}, () => [
-              _hoisted_2,
-              _hoisted_3
-            ])
-          ], 16, _hoisted_1)
-        ], 2)) : createCommentVNode("", true),
-        (openBlock(true), createElementBlock(Fragment, null, renderList(slotProps.computed.pageRange, (page, key) => {
-          return openBlock(), createElementBlock("li", {
-            class: normalizeClass(["page-item pagination-page-nav", { "active": page == slotProps.computed.currentPage }]),
-            key
+      slotProps.themeEngine == "tailwind" ? (openBlock(), createElementBlock("div", _hoisted_1, [
+        slotProps.computed.total > slotProps.computed.perPage ? (openBlock(), createElementBlock("ul", mergeProps({ key: 0 }, _ctx.$attrs, {
+          class: ["pagination inline-flex -space-x-px", {
+            "pagination-sm": slotProps.size == "small",
+            "pagination-lg": slotProps.size == "large",
+            "justify-content-center": slotProps.align == "center",
+            "justify-content-end": slotProps.align == "right"
+          }]
+        }), [
+          slotProps.computed.prevPageUrl || slotProps.showDisabled ? (openBlock(), createElementBlock("li", {
+            key: 0,
+            class: normalizeClass(["page-item pagination-prev-nav", { "disabled": !slotProps.computed.prevPageUrl }])
+          }, [
+            createElementVNode("a", mergeProps({
+              "aria-label": "Previous",
+              tabindex: !slotProps.computed.prevPageUrl && -1
+            }, toHandlers(slotProps.prevButtonEvents), {
+              href: "#",
+              class: "page-linkblock py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            }), _hoisted_5, 16, _hoisted_2)
+          ], 2)) : createCommentVNode("", true),
+          (openBlock(true), createElementBlock(Fragment, null, renderList(slotProps.computed.pageRange, (page, key) => {
+            return openBlock(), createElementBlock("li", {
+              class: normalizeClass(["page-item pagination-page-nav", { "active": page == slotProps.computed.currentPage }]),
+              key
+            }, [
+              createElementVNode("a", mergeProps({
+                class: "page-link py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white",
+                href: "#"
+              }, toHandlers(slotProps.pageButtonEvents(page))), toDisplayString(page), 17)
+            ], 2);
+          }), 128)),
+          slotProps.computed.nextPageUrl || slotProps.showDisabled ? (openBlock(), createElementBlock("li", {
+            key: 1,
+            class: normalizeClass(["page-item pagination-next-nav", { "disabled": !slotProps.computed.nextPageUrl }])
+          }, [
+            createElementVNode("a", mergeProps({
+              href: "#",
+              "aria-label": "Next",
+              tabindex: !slotProps.computed.nextPageUrl && -1
+            }, toHandlers(slotProps.nextButtonEvents), { class: "page-link block py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" }), _hoisted_9, 16, _hoisted_6)
+          ], 2)) : createCommentVNode("", true)
+        ], 16)) : createCommentVNode("", true)
+      ])) : (openBlock(), createElementBlock("div", _hoisted_10, [
+        slotProps.computed.total > slotProps.computed.perPage ? (openBlock(), createElementBlock("ul", mergeProps({ key: 0 }, _ctx.$attrs, {
+          class: ["pagination", {
+            "pagination-sm": slotProps.size == "small",
+            "pagination-lg": slotProps.size == "large",
+            "justify-content-center": slotProps.align == "center",
+            "justify-content-end": slotProps.align == "right"
+          }]
+        }), [
+          slotProps.computed.prevPageUrl || slotProps.showDisabled ? (openBlock(), createElementBlock("li", {
+            key: 0,
+            class: normalizeClass(["page-item pagination-prev-nav", { "disabled": !slotProps.computed.prevPageUrl }])
           }, [
             createElementVNode("a", mergeProps({
               class: "page-link",
-              href: "#"
-            }, toHandlers(slotProps.pageButtonEvents(page))), [
-              createTextVNode(toDisplayString(page) + " ", 1),
-              page == slotProps.computed.currentPage ? (openBlock(), createElementBlock("span", _hoisted_4, "(current)")) : createCommentVNode("", true)
-            ], 16)
-          ], 2);
-        }), 128)),
-        slotProps.computed.nextPageUrl || slotProps.showDisabled ? (openBlock(), createElementBlock("li", {
-          key: 1,
-          class: normalizeClass(["page-item pagination-next-nav", { "disabled": !slotProps.computed.nextPageUrl }])
-        }, [
-          createElementVNode("a", mergeProps({
-            class: "page-link",
-            href: "#",
-            "aria-label": "Next",
-            tabindex: !slotProps.computed.nextPageUrl && -1
-          }, toHandlers(slotProps.nextButtonEvents)), [
-            renderSlot(_ctx.$slots, "next-nav", {}, () => [
-              _hoisted_6,
-              _hoisted_7
-            ])
-          ], 16, _hoisted_5)
-        ], 2)) : createCommentVNode("", true)
-      ], 16)) : createCommentVNode("", true)
+              href: "#",
+              "aria-label": "Previous",
+              tabindex: !slotProps.computed.prevPageUrl && -1
+            }, toHandlers(slotProps.prevButtonEvents)), [
+              renderSlot(_ctx.$slots, "prev-nav", {}, () => [
+                _hoisted_12,
+                _hoisted_13
+              ])
+            ], 16, _hoisted_11)
+          ], 2)) : createCommentVNode("", true),
+          (openBlock(true), createElementBlock(Fragment, null, renderList(slotProps.computed.pageRange, (page, key) => {
+            return openBlock(), createElementBlock("li", {
+              class: normalizeClass(["page-item pagination-page-nav", { "active": page == slotProps.computed.currentPage }]),
+              key
+            }, [
+              createElementVNode("a", mergeProps({
+                class: "page-link",
+                href: "#"
+              }, toHandlers(slotProps.pageButtonEvents(page))), [
+                createTextVNode(toDisplayString(page) + " ", 1),
+                page == slotProps.computed.currentPage ? (openBlock(), createElementBlock("span", _hoisted_14, "(current)")) : createCommentVNode("", true)
+              ], 16)
+            ], 2);
+          }), 128)),
+          slotProps.computed.nextPageUrl || slotProps.showDisabled ? (openBlock(), createElementBlock("li", {
+            key: 1,
+            class: normalizeClass(["page-item pagination-next-nav", { "disabled": !slotProps.computed.nextPageUrl }])
+          }, [
+            createElementVNode("a", mergeProps({
+              class: "page-link",
+              href: "#",
+              "aria-label": "Next",
+              tabindex: !slotProps.computed.nextPageUrl && -1
+            }, toHandlers(slotProps.nextButtonEvents)), [
+              renderSlot(_ctx.$slots, "next-nav", {}, () => [
+                _hoisted_16,
+                _hoisted_17
+              ])
+            ], 16, _hoisted_15)
+          ], 2)) : createCommentVNode("", true)
+        ], 16)) : createCommentVNode("", true)
+      ]))
     ]),
     _: 3
   }, 8, ["data", "limit", "show-disabled", "size", "align", "onPaginationChangePage"]);
--- src/LaravelVuePagination.vue
+++ src/LaravelVuePagination.vue
@@ -8,43 +8,82 @@
         @pagination-change-page="onPaginationChangePage"
         v-slot="slotProps"
     >
-        <ul
-            v-bind="$attrs"
-            class="pagination"
-            :class="{
-                'pagination-sm': slotProps.size == 'small',
-                'pagination-lg': slotProps.size == 'large',
-                'justify-content-center': slotProps.align == 'center',
-                'justify-content-end': slotProps.align == 'right'
-            }"
-            v-if="slotProps.computed.total > slotProps.computed.perPage">
-
-            <li class="page-item pagination-prev-nav" :class="{'disabled': !slotProps.computed.prevPageUrl}" v-if="slotProps.computed.prevPageUrl || slotProps.showDisabled">
-                <a class="page-link" href="#" aria-label="Previous" :tabindex="!slotProps.computed.prevPageUrl && -1" v-on="slotProps.prevButtonEvents">
-                    <slot name="prev-nav">
-                        <span aria-hidden="true">&laquo;</span>
+        <div v-if="slotProps.themeEngine == 'tailwind'">
+            <ul
+                v-bind="$attrs"
+                class="pagination inline-flex -space-x-px"
+                :class="{
+                    'pagination-sm': slotProps.size == 'small',
+                    'pagination-lg': slotProps.size == 'large',
+                    'justify-content-center': slotProps.align == 'center',
+                    'justify-content-end': slotProps.align == 'right'
+                }"
+                v-if="slotProps.computed.total > slotProps.computed.perPage">
+                
+                <li class="page-item pagination-prev-nav" :class="{'disabled': !slotProps.computed.prevPageUrl}" v-if="slotProps.computed.prevPageUrl || slotProps.showDisabled">
+                    <a aria-label="Previous" :tabindex="!slotProps.computed.prevPageUrl && -1" v-on="slotProps.prevButtonEvents" href="#" class=" page-linkblock py-2 px-3 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                         <span class="sr-only">Previous</span>
-                    </slot>
-                </a>
-            </li>
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd"></path></svg>
+                    </a>
+                </li>
+                    
+                
 
-            <li class="page-item pagination-page-nav" v-for="(page, key) in slotProps.computed.pageRange" :key="key" :class="{ 'active': page == slotProps.computed.currentPage }">
-                <a class="page-link" href="#" v-on="slotProps.pageButtonEvents(page)">
-                    {{ page }}
-                    <span class="sr-only" v-if="page == slotProps.computed.currentPage">(current)</span>
-                </a>
-            </li>
+                <li class="page-item pagination-page-nav" v-for="(page, key) in slotProps.computed.pageRange" :key="key" :class="{ 'active': page == slotProps.computed.currentPage }">
+                    <a class="page-link py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white" href="#" v-on="slotProps.pageButtonEvents(page)">
+                        {{ page }}
+                    </a>
+                </li>
 
-            <li class="page-item pagination-next-nav" :class="{'disabled': !slotProps.computed.nextPageUrl}" v-if="slotProps.computed.nextPageUrl || slotProps.showDisabled">
-                <a class="page-link" href="#" aria-label="Next" :tabindex="!slotProps.computed.nextPageUrl && -1" v-on="slotProps.nextButtonEvents">
-                    <slot name="next-nav">
-                        <span aria-hidden="true">&raquo;</span>
+                <li class="page-item pagination-next-nav" :class="{'disabled': !slotProps.computed.nextPageUrl}" v-if="slotProps.computed.nextPageUrl || slotProps.showDisabled">
+                    <a href="#" aria-label="Next" :tabindex="!slotProps.computed.nextPageUrl && -1" v-on="slotProps.nextButtonEvents" class="page-link block py-2 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
                         <span class="sr-only">Next</span>
-                    </slot>
-                </a>
-            </li>
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                    </a>
+                </li>
 
-        </ul>
+            </ul>
+        </div>
+        <div v-else>
+            <ul
+                v-bind="$attrs"
+                class="pagination"
+                :class="{
+                    'pagination-sm': slotProps.size == 'small',
+                    'pagination-lg': slotProps.size == 'large',
+                    'justify-content-center': slotProps.align == 'center',
+                    'justify-content-end': slotProps.align == 'right'
+                }"
+                v-if="slotProps.computed.total > slotProps.computed.perPage">
+
+                <li class="page-item pagination-prev-nav" :class="{'disabled': !slotProps.computed.prevPageUrl}" v-if="slotProps.computed.prevPageUrl || slotProps.showDisabled">
+                    <a class="page-link" href="#" aria-label="Previous" :tabindex="!slotProps.computed.prevPageUrl && -1" v-on="slotProps.prevButtonEvents">
+                        <slot name="prev-nav">
+                            <span aria-hidden="true">&laquo;</span>
+                            <span class="sr-only">Previous</span>
+                        </slot>
+                    </a>
+                </li>
+
+                <li class="page-item pagination-page-nav" v-for="(page, key) in slotProps.computed.pageRange" :key="key" :class="{ 'active': page == slotProps.computed.currentPage }">
+                    <a class="page-link" href="#" v-on="slotProps.pageButtonEvents(page)">
+                        {{ page }}
+                        <span class="sr-only" v-if="page == slotProps.computed.currentPage">(current)</span>
+                    </a>
+                </li>
+
+                <li class="page-item pagination-next-nav" :class="{'disabled': !slotProps.computed.nextPageUrl}" v-if="slotProps.computed.nextPageUrl || slotProps.showDisabled">
+                    <a class="page-link" href="#" aria-label="Next" :tabindex="!slotProps.computed.nextPageUrl && -1" v-on="slotProps.nextButtonEvents">
+                        <slot name="next-nav">
+                            <span aria-hidden="true">&raquo;</span>
+                            <span class="sr-only">Next</span>
+                        </slot>
+                    </a>
+                </li>
+
+            </ul>
+        </div>
+        
     </RenderlessLaravelVuePagination>
 </template>
 
@@ -72,6 +111,10 @@ export default {
         showDisabled: {
             type: Boolean,
             default: false
+        },
+        themeEngine: {
+            type: String,
+            default: "tailwind"
         },
         size: {
             type: String,
